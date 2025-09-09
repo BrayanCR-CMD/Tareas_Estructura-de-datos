5114ac73ddce421e0798c1fd26494d3bc3299ded
BrayanCR-CMD <brayanenriquecontrerasreyes@gmail.com>
2025-09-08 22:37:12 -0700

    Update Busquedalineal.java

--- Busquedalineal.java
+++ Busquedalineal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Busquedalineal{
-
+    //funcion de busqueda
     public static int busqueda(String [] Nombres, String buscar, int largo){
     for (int i=0; i < largo; i++){
         if (Nombres[i].equals(buscar)){
@@ -12,10 +12,14 @@ public class Busquedalineal{
 }
 
     public static void main(String[] args){
+
+        //declaracion de variables, arreglos y scanner
         Scanner sc = new Scanner(System.in);
         String [] Nombres = new String[5];
         String buscar;
         int largo = Nombres.length;
+        //llenar el arreglo
+        
         for (int i=0; i < Nombres.length; i++){
             System.out.println("ingrese el nombre");
             Nombres[i] = sc.nextLine();
@@ -23,6 +27,7 @@ public class Busquedalineal{
         System.out.println("ingrese un nombre que dese buscar");
         buscar = sc.nextLine();
 
+        //lamar a la fincion y imprimir el resultado
         if (busqueda(Nombres, buscar, largo) == -1){
             System.out.println("el nombre no se encontro");
         }else{

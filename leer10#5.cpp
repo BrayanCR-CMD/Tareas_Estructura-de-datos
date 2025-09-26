09e6640965a90a443b48a358e0e663d83154a889
BrayanCR-CMD <brayanenriquecontrerasreyes@gmail.com>
2025-09-26 15:53:24 -0700

    Update leer10#5.cpp

--- leer10#5.cpp
+++ leer10#5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+//proceso para invertir el orden del arreglo
 int orden_inv(int n, int numeros[]){
     int j = 0;
     for(int i = (n-1); i>4; i--){
@@ -10,26 +11,31 @@ int orden_inv(int n, int numeros[]){
 
     }
 }
-int main(){
-    int n = 10;
-    int numeros[10];
-
+//proceso para mostrar el arreglo
+void escribir_nums(int n, int numeros[]){
+    for(int i = 0; i < n; i++){
+        cout << "el numero en la posicion " << i << " es: " << numeros[i] << endl;
+    }
+    cout << "\n";
+}
+//proceso para capturar los numeros
+int capturar_nums(int n, int numeros[]){
     for (int i = 0; i < n; i++){
         system("cls");
 
         cout << "ingrese un numero" << endl;
         cin >> numeros[i];
     }
+}
+//bloque de codigo principal
+int main(){
+    // variables
+    int n = 10;
+    int numeros[10];
 
-    for(int i = 0; i < n; i++){
-        cout << "el numero en la posicion " << i << " es: " << numeros[i] << endl;
-    }
-    cout << "\n";
-
+    capturar_nums(n,numeros);
+    escribir_nums(n,numeros);
     cout << "ahora invertimos el orden \n" << endl;
     orden_inv(n, numeros);
-    for(int i = 0; i < n; i++){
-        cout << "el numero en la posicion " << i << " es: " << numeros[i] << endl;
-    }
-
+    escribir_nums(n,numeros);
 }

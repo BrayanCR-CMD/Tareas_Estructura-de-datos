7f415ce7fbfd17fefd7d3a5e2f11c1c721169eb0
BrayanCR-CMD <brayanenriquecontrerasreyes@gmail.com>
2025-10-05 16:29:00 -0700

    Create metodo_quicksort.java

---
+++ metodo_quicksort.java
@@ -0,0 +1,46 @@
+public class metodo_quicksort {
+    public static void quicksort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quicksort(array, low, pivotIndex - 1);
+            quicksort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {34, 7, 23, 32, 5, 62};
+        System.out.println("Array original:");
+        printArray(array);
+
+        quicksort(array, 0, array.length - 1);
+
+        System.out.println("Array ordenado:");
+        printArray(array);
+    }
+
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}

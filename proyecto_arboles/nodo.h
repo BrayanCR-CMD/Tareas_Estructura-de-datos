eecfa7dc5981633371f5e62b75ff6bc08fe37cd0
BrayanCR-CMD <brayanenriquecontrerasreyes@gmail.com>
2025-12-15 10:13:37 -0700

    Refactor Nodo class to NodoAvanzado with enhancements

--- proyecto_arboles/nodo.h
+++ proyecto_arboles/nodo.h
@@ -1,30 +1,46 @@
 #ifndef NODO_H_INCLUDED
 #define NODO_H_INCLUDED
 
-// nodo.hpp
+// nodo
 
 #include <memory>
 #include <vector>
 #include <string>
+#include <ctime>
 
-class Nodo : public std::enable_shared_from_this<Nodo> {
+class NodoAvanzado : public std::enable_shared_from_this<NodoAvanzado> {
 public:
     int id;
     std::string nombre;
     std::string tipo; // "carpeta" o "archivo"
     std::string contenido;
-    std::vector<std::shared_ptr<Nodo>> hijos;
-    std::shared_ptr<Nodo> padre;
-
-    Nodo(int id, std::string nombre, std::string tipo, std::shared_ptr<Nodo> padre = nullptr);
-
-    void agregarHijo(std::shared_ptr<Nodo> hijo);
+    std::vector<std::shared_ptr<NodoAvanzado>> hijos;
+    std::shared_ptr<NodoAvanzado> padre;
+    time_t fechaCreacion;
+    time_t fechaModificacion;
+    size_t tamaño;
+    bool oculto;
+    
+    NodoAvanzado(int id, std::string nombre, std::string tipo, 
+                std::shared_ptr<NodoAvanzado> padre = nullptr);
+    
+    // Métodos públicos
+    void agregarHijo(std::shared_ptr<NodoAvanzado> hijo);
     bool eliminarHijo(std::string nombreHijo);
-    std::shared_ptr<Nodo> buscarHijo(std::string nombreHijo);
-    std::string obtenerRutaCompleta();
-
+    std::shared_ptr<NodoAvanzado> buscarHijo(std::string nombreHijo, bool incluirOcultos = false);
+    std::string obtenerRutaCompleta() const;
+    std::string obtenerInfo() const;
+    
+    // Métodos de gestión
+    void actualizarFechaModificacion();
+    void actualizarFechaAcceso();
+    void actualizarTamaño();
+    bool setContenido(const std::string& nuevoContenido);
+    void marcarOculto(bool esOculto);
+    
 private:
-    // M�todos auxiliares privados
+    std::string formatoTamaño(size_t bytes) const;
 };
 
 #endif // NODO_H_INCLUDED
+

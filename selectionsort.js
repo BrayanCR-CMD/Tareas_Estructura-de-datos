63662b2e66a0bcbbc318d31ed098e285bcd3f378
BrayanCR-CMD <brayanenriquecontrerasreyes@gmail.com>
2026-01-10 15:29:30 -0700

    Create selectionsort.js

---
+++ selectionsort.js
@@ -0,0 +1,22 @@
+//metodo de ordenamiento selection sort
+function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+// Ejemplo de uso
+let array = [64, 25, 12, 22, 11];
+console.log("Array original:", array);
+let sortedArray = selectionSort(array);
+console.log("Array ordenado:", sortedArray);
+return 0;
